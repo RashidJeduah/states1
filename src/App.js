3fc2885c6ce8d27c57a5a8271ce69e815b488de2
Jeduah Rashid <jeduahrashid@gmail.com>
2020-09-25 10:29:01 +0000

    states first exercise

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    heading:'This is a States exercise'
+  }
+
+  changeHeadingState =() => {
+    this.setState({heading:'States is complicated'});
+  }
+  render() {
+    return (
+      <>
+        <h1> {this.state.heading} </h1>
+        <button onClick={this.changeHeadingState}>enter</button>
+      </>
+    )
+  }
 }
 
 export default App;
